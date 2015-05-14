d83f3c17a8ead6167e2f614318577b8a901e9efb
ggbioing <luigi.antelmi@gmail.com>
2015-05-14 12:54:22 +0200

    add general description

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit
+## to caching the inverse of a matrix rather than compute it repeatedly
+## In this script you can find a pair of functions that cache the inverse of a matrix.
 
 ## Write a short comment describing this function
 
